1a754b18953099350188da94c008a3bc8536b1e5
JONGWOO JIN <co_pa_ne@JONGWOOui-MacBook-Pro.local>
2018-11-26 15:16:10 +0900

    通知を受信した時の挙動の修正

--- app/src/main/java/aws_push_notification/group4/amazonmobilepush/CustomFirebaseMessagingService.java
+++ app/src/main/java/aws_push_notification/group4/amazonmobilepush/CustomFirebaseMessagingService.java
@@ -1,12 +1,17 @@
 package aws_push_notification.group4.amazonmobilepush;
 
+//import android.app.Notification;
+//import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+//import android.graphics.Color;
 import android.media.RingtoneManager;
 import android.net.Uri;
+//import android.os.Build;
+import android.os.PowerManager;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
@@ -26,14 +31,27 @@ public class CustomFirebaseMessagingService extends com.google.firebase.messagin
         String title = "AmazonMobilePush";//data.get("title");
         String message = data.get("default");
 
-        sendNotification(title, message);
+        try{
+
+            PowerManager.WakeLock wakelock = ((PowerManager) this.getSystemService(Context.POWER_SERVICE)).newWakeLock(PowerManager.ACQUIRE_CAUSES_WAKEUP | PowerManager.FULL_WAKE_LOCK | PowerManager.ON_AFTER_RELEASE,"disableLock");
+            wakelock.acquire();
+            sendNotification(title, message);
+            wakelock.release();
+
+        }catch (Exception e) {
+            sendNotification(title, message);
+            Log.i(TAG, "Exception::" + e.toString());
+        }
     }
 
     private void sendNotification(String title, String message) {
+
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
                 PendingIntent.FLAG_CANCEL_CURRENT);
+
 
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
